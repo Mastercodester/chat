3edd74eb7a8e3d4a50cdb1c059c3555474741463
code <ajacks75@emich.edu>
2022-12-12 16:22:02 -0500

    Initial Commit

---
+++ swiftchat/swiftchatApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftchatApp.swift
+//  swiftchat
+//
+//  Created by Sean Carter on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftchatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
